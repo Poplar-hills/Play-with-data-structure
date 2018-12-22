eebd2b0742ffe7c84ad4a7b18289583cf686250e
Mengyang Jiang <maxjiang23326@gmail.com>
2018-12-22 16:54:04 +0800

    feat: add comments for LinkedList

--- src/main/java/LinkedList/L203_Remove_Linked_List_Elements/Solution3.java
+++ src/main/java/LinkedList/L203_Remove_Linked_List_Elements/Solution3.java
@@ -4,10 +4,7 @@ package LinkedList.L203_Remove_Linked_List_Elements;
 * 用递归解决。
 * - 链表天然具有递归性质：一个 1->2->3->4->null 的链表可以看做是头结点 1 后面挂了一个 2->3->4->null 的链表；
 *   而 2->3->4 同样可以看做是 2 后面挂了一个 3->4->null 的链表，以此类推。因此，近乎所有和链表相关的操作都可以用递归的形式完成。
-* - 递归的本质（SEE: https://coding.imooc.com/lesson/207.html#mid=13435，11'36''）：
-*   1. 将原问题不断转化为更小的问题
-*   2. 求解最基本的问题
-* - 以递归的方式思考，关键是要识别出：
+* - 以递归的方式思考关键是要识别出：
 *   1. 递归的终止条件（在哪里停止递归）
 *   2. 递归的最小重复单元（重复哪部分操作）
 * - 该问题中：
@@ -20,6 +17,6 @@ public class Solution3 implements Solution {
     public ListNode removeElements(ListNode head, int val) {
         if (head == null) return null;
         head.next = removeElements(head.next, val);
-        return head.val == val ? head.next : head;
+        return head.val == val ? head.next: head;
     }
 }
--- src/main/java/LinkedList/LinkedList.java
+++ src/main/java/LinkedList/LinkedList.java
@@ -4,12 +4,15 @@ package LinkedList;
  * - 注：先看 LinkedListWithoutDummyHead.java，再看这里的实现。
  * - LinkedListWithoutDummyHead 的实现没有问题，只是不够优雅，因为在对任意位置添加结点的时候需要区别 index 是否是 0。
  * - 解决办法是通过在 LinkedList 最前面添加一个虚拟结点（dummyHead）来统一两种情况，这是一种常见技巧。
- *
  * - 链表的时间复杂度分析：
- *   - 对于头尾增、删操作（addLast/removeLast），链表和数组的复杂度刚好相反：
- *       - 对于数组，尾部操作是 O(1)，头部操作是 O(n)
- *       - 对于链表，尾部操作是 O(n)，头部操作是 O(1)
- *   - 对于任意位置的操作（addAtIndex/removeAtIndex）链表是 O(n/2)，所以也是 O(n)，而数组是 O(1)。
+ *   - 增/删操作：
+ *     - 对于数组来说：
+ *       - 对尾部的操作（addLast/removeLast）是 O(1)
+ *       - 对头部的操作（addFirst/removeFirst）是 O(n)
+ *     - 而对于链表来说正相反：
+ *       - 对头部的操作（addFirst/removeFirst）是 O(1)
+ *       - 对尾部的操作（addLast/removeLast）是 O(n)
+ *       - 任意位置的操作（addAtIndex/removeAtIndex）是 O(n/2)，所以也是 O(n)
  *   - 所以综合来看，链表的增、删操作的复杂度都是 O(n)
  *   - 对于改操作（set），因为链表不支持随机访问，因此是其复杂度是 O(n)
  *   - 对于查操作（contains, get），都需要遍历链表，因此复杂度都是 O(n)
@@ -21,9 +24,6 @@ package LinkedList;
  *   1. 因为链表对于头部的效率是 O(1)，因此非常适合作为栈的底层实现（因为栈只在一端进行操作）；
  *   2. 链表也可以作为 Queue 的底层实现，但需要进行改造（增加一个 tail 指针，使得 dequeue 操作的复杂度也是 O(1)），
  *      具体实现 SEE: LinkedListQueue.java
- *
- * - 链表与递归：
- *   链表具有天然的 1.动态性质 2.递归性质。其递归性质 SEE：L203_Remove_Linked_List_Elements - Solution3
  * */
 
 import Array.Array;
@@ -149,13 +149,12 @@ public class LinkedList<E> {
     public E removeLast() { return removeAtIndex(size - 1); }
 
     public void removeElement(E e) {
-        if (e == null)
-            throw new IllegalArgumentException("removeElement failed.");
         dummyHead = removeElement(dummyHead, e);
     }
 
-    private Node removeElement(Node node, E e) {  // 不同于 remove，该方法不再需要 prev 节点，只需要判断当前节点是否是待删除节点，若是的话直接断开与下个节点的链接，并返回下个节点即可（不需要将下个节点连接到 prev 上）。
-        if (node == null) return null;
+    private Node removeElement(Node node, E e) {  // 不同于 remove，该方法不再需要获取 prev 节点，只需要判断当前节点是否是待删除节点，如果是的话直接断开与下个节点的链接，并返回下个节点即可。
+        if (node == null)
+            return null;
         if (node.e != null && node.e.equals(e)) {
             Node next = node.next;
             node.next = null;
@@ -178,23 +177,6 @@ public class LinkedList<E> {
             }
             prev = prev.next;
         }
-    }
-
-    public void removeElements(E e) {  // 删除链表中所有值为 e 的节点
-        if (e == null)
-            throw new IllegalArgumentException("removeElements failed.");
-        dummyHead = removeElements(dummyHead, e);
-    }
-
-    private Node removeElements(Node node, E e) {  // 不同于上面写的所有递归，这个方法的递归方式是先一路往下递归到最后一个元素，然后在不断 return 回来的路上进行判断，这种递归适合需要遍历所有元素的情况。
-        if (node == null) return null;
-        node.next = removeElements(node.next, e);
-
-        if (node.e != null && node.e.equals(e)) {
-            size--;
-            return node.next;
-        }
-        return node;
     }
 
     /*
