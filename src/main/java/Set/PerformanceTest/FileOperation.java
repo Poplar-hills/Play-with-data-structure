341c4f38dd2e578e50777e2088781650132b891e
Max Jiang <maxjiang23326@gmail.com>
2019-03-04 23:53:31 +0800

    refactor: drop empty lines

--- src/main/java/Set/PerformanceTest/FileOperation.java
+++ src/main/java/Set/PerformanceTest/FileOperation.java
@@ -15,7 +15,6 @@ public class FileOperation {
 
     // 读取文件名称为filename中的内容，并将其中包含的所有词语放进words中
     public static boolean readFile(String filename, ArrayList<String> words){
-
         if (filename == null || words == null){
             System.out.println("filename is null or words is null");
             return false;
@@ -25,7 +24,6 @@ public class FileOperation {
         Scanner scanner;
 
         try {
-
             File file = new File(filename);
             if(file.exists()){
                 FileInputStream fis = new FileInputStream(file);
@@ -45,9 +43,7 @@ public class FileOperation {
         // 这个分词方式相对简陋, 没有考虑很多文本处理中的特殊问题
         // 在这里只做demo展示用
         if (scanner.hasNextLine()) {
-
             String contents = scanner.useDelimiter("\\A").next();
-
             int start = firstCharacterIndex(contents, 0);
             for (int i = start + 1; i <= contents.length(); )
                 if (i == contents.length() || !Character.isLetter(contents.charAt(i))) {
@@ -58,13 +54,11 @@ public class FileOperation {
                 } else
                     i++;
         }
-
         return true;
     }
 
     // 寻找字符串s中，从start的位置开始的第一个字母字符的位置
     private static int firstCharacterIndex(String s, int start){
-
         for( int i = start ; i < s.length() ; i ++ )
             if( Character.isLetter(s.charAt(i)) )
                 return i;
